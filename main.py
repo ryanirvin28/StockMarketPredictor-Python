cbdc802b8e77cfc9424664be6aa488c69e95d7ea
Ryan Irvin <ryanirvin28@gmail.com>
2020-10-03 22:58:41 -0500

    testing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import quandl
+import quandl, math
 import pandas
 import numpy
+import datetime
 
-from sklearn.linear_model import LinearRegression
-from sklearn import preprocessing, svm
-from sklearn.model_selection import train_test_split
 
+df = quandl.get("WIKI/AMZN")
+print(df.tail())
+
+print(df.head(2).round(1))
+print("\n")
+
+print(df.columns)
+print(df.index)
