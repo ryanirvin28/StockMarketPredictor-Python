c129773d7af617fd2bf8a430c41914c2dde1a115
Ryan Irvin <ryanirvin28@gmail.com>
2020-10-03 23:38:08 -0500

    print adjusted df

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ print("\n")
 
 print(df.columns)
 print(df.index)
+
+df = df[('Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume')]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low'])/(df['Adj. Low']*100)
+df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open'])/(df['Adj. Open']*100)
+
+df = df[['Adj. Close', 'HCL_PCT', 'PCT_Change', 'Adj. Volume']]
+print(df.head())
