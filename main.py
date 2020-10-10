15f163141833842224d71da290f14a2e1bd3250c
Ryan Irvin <ryanirvin28@gmail.com>
2020-10-10 02:57:30 -0500

    Addition of linear regression, currently using Amazon and not the correct date. To-Do: create graphic change to S&P 500 update the date

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import quandl, math
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 import datetime
+
+from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
 
 
 df = quandl.get("WIKI/AMZN")
+
+
+df = df[['Adj. Close']]
+
+forecast_out = int(30)
+df['Prediction'] = df[['Adj. Close']].shift(-forecast_out)
+
 print(df.tail())
 
-print(df.head(2).round(1))
-print("\n")
+x = np.array(df.drop(['Prediction'], 1))
+x = preprocessing.scale(x)
 
-print(df.columns)
-print(df.index)
+x_forecast = x[-forecast_out:]
+x = x[:-forecast_out]
 
-df = df[('Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume')]
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low'])/(df['Adj. Low']*100)
-df['PCT_Change'] = (df['Adj. Close'] - df['Adj. Open'])/(df['Adj. Open']*100)
+y = np.array(df['Prediction'])
+y = y[:-forecast_out]
 
-df = df[['Adj. Close', 'HCL_PCT', 'PCT_Change', 'Adj. Volume']]
-print(df.head())
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
+
+clf = LinearRegression()
+clf.fit(x_train, y_train)
+
+confidence = clf.score(x_test, y_test)
+print("Confidence: ", confidence)
+
+forecast_prediction = clf.predict(x_forecast)
+print(forecast_prediction)
+
